537457f99428d36ea6b933426f17e40a33edec8c
Ahmed Raza Shahid <mr.sherry6967@gmail.com>
2025-02-25 11:33:34 +0500

    events

--- events/index.js
+++ events/index.js
@@ -1,8 +1,30 @@
-let images=document.getElementById("list-images").addEventListener("click",function (e) {
-    console.log(e.target.parentNode);
-        console.log(e.target.tagName);
-        if (e.target.tagName === "IMG") {
-            e.target.parentNode.remove();
-            alert("Removing clicked picture ")
+
+let imageUrlInput = document.getElementById('imageUrlInput');
+let addBtn = document.getElementById('addImageBtn');
+let listImages = document.getElementById('list-images');
+
+    
+    listImages.addEventListener('click', function(e) {
+        if (e.target.tagName ==="IMG") {
+            e.target.parentNode.remove(); 
+            alert("Removing clicked picture");
         }
-})
+    });
+
+addBtn.addEventListener('click', function() {
+    let imageUrl = imageUrlInput.value;
+
+    if (imageUrl) {
+        let li = document.createElement('li');
+        
+        let img = document.createElement('img');
+        img.src = imageUrl;
+
+        li.appendChild(img);
+        listImages.appendChild(li);
+
+    } 
+    else {
+        alert('Please enter image URL.');
+    }
+});
